1972ea520d8f06fe452966755a80c421e0ac51e8
tintin89 <70333598+tintin89@users.noreply.github.com>
2021-01-15 16:25:44 -0400

    one commit
    Clon básico de netflixx aprendiendo...

---
+++ src/Nav.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleshow] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleshow(true)
+      } else handleshow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll")
+    }
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <div
+        className="nav_logo"
+        
+      >NetflixTintin-basicClone</div>
+      <img
+        className="nav_avatar"
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+        alt=""
+      />
+    </div>
+  )
+}
+
+export default Nav
